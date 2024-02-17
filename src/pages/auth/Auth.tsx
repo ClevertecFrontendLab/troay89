f732940bdc210b18e4eb2bdef2e74c5dc98f7477
Home <picccc89@gmail.com>
2024-02-17 12:12:59 +0300

    feat: made registration

--- src/pages/auth/Auth.tsx
+++ src/pages/auth/Auth.tsx
@@ -2,10 +2,7 @@ import React, { useState } from 'react';
 import { Button, Card, Checkbox, Image, Input, Layout, Space, Typography } from 'antd';
 import type { CheckboxChangeEvent } from 'antd/es/checkbox';
 import logoAuth from '/img/svg/logo-auth.svg';
-import {
-    GooglePlusOutlined
-
-} from "@ant-design/icons";
+import { GooglePlusOutlined } from '@ant-design/icons';
 import './Auth.css';
 
 const tabList = [
@@ -29,30 +26,51 @@ const contentList: Record<string, React.ReactNode> = {
     tab1: (
         <Space direction='vertical'>
             <Space direction='vertical'>
-                <Input className={'auth-input auth-input-email'} size={"large"} addonBefore='e-mail:' />
+                <Input
+                    className={'auth-input auth-input-email'}
+                    size={'large'}
+                    addonBefore='e-mail:'
+                />
                 <Input.Password className={'auth-input'} placeholder='Пaроль' />
             </Space>
             <Space className={'extra-container'}>
-                <Checkbox onChange={onChange}>
-                    Запомнить меня
-                </Checkbox>
+                <Checkbox onChange={onChange}>Запомнить меня</Checkbox>
                 <Link className={'forgot-link'} href='https://ant.design' target='_blank'>
                     Забыли пароль?
                 </Link>
             </Space>
             <Space className={'container-auth-buttons'} direction={'vertical'}>
-                <Button className={'auth-enter'} type='primary'>Войти</Button>
-                <Button className={'auth-enter'} icon={<GooglePlusOutlined />}>Войти через Google</Button>
+                <Button className={'auth-enter'} type='primary'>
+                    Войти
+                </Button>
+                <Button className={'auth-enter'} icon={<GooglePlusOutlined />}>
+                    Войти через Google
+                </Button>
             </Space>
         </Space>
     ),
-    tab2:<Space direction='vertical'>
-        <Space className={'container-input-reg'} direction='vertical'>
-            <Input className={'auth-input auth-input-email'} size={"large"} addonBefore='e-mail:' />
-            <Input.Password className={'auth-input'} placeholder='Пaроль' />
+    tab2: (
+        <Space direction='vertical' style={{ gap: 0 }}>
+            <Space className={'container-input-reg'} direction='vertical'>
+                <Input
+                    className={'reg-input reg-input-email'}
+                    size={'large'}
+                    addonBefore='e-mail:'
+                />
+                <Input.Password className={'reg-input'} placeholder='Пaроль' />
+            </Space>
+            <span className={'password-message'}>
+                Пароль не менее 8 символов, с заглавной буквой и цифрой
+            </span>
+            <Input.Password className={'reg-input repeat-pass'} placeholder='Повторите пaроль' />
+            <Button className={'reg-enter'} type='primary'>
+                Войти
+            </Button>
+            <Button className={'auth-enter'} icon={<GooglePlusOutlined />}>
+                Регистрация через Google
+            </Button>
         </Space>
-    </Space>
-
+    ),
 };
 
 export const Auth: React.FC = () => {
@@ -68,7 +86,7 @@ export const Auth: React.FC = () => {
             <Content className={'container-auth'}>
                 <Card
                     bordered={false}
-                    className={'auth-card'}
+                    className={`auth-card ${activeTabKey1 === 'tab2' ? 'reg-card' : ''}`}
                     title={<Image src={logoAuth} alt={'logo for auth'} className={'logo-auth'} />}
                     tabList={tabList}
                     activeTabKey={activeTabKey1}
