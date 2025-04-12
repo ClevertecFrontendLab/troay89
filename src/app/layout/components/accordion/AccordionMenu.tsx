ca9a0d42a0a464e7541d1242bb48f335290b1bf4
troay89 <picccc89@gmail.com>
2025-04-12 22:49:19 +0300

    fix(scroll): customized scroll

--- src/app/layout/components/accordion/AccordionMenu.tsx
+++ src/app/layout/components/accordion/AccordionMenu.tsx
@@ -50,7 +50,9 @@ function AccordionMenu() {
                         <List className={styles['list']} spacing={2}>
                             {items.map((item) => (
                                 <ListItem className={styles['list_item']} key={item}>
-                                    <Box className={styles['vert_line']} />
+                                    <Box
+                                        className={`${styles['vert_line']} ${item === 'Вторые блюда' && location.pathname === '/vegan' ? styles['line_change'] : ''}`}
+                                    />
                                     <Link
                                         className={`${styles['item_link']} ${item === 'Вторые блюда' && location.pathname === '/vegan' ? styles['item_change'] : ''}`}
                                     >
